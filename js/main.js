519a19dffb4b4520c12a4c485c0670f940c0f9c3
btlenny <btlenny@gmail.com>
2023-09-21 10:48:34 -0700

    Initial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Create an array to store the sequence of colors and sounds that Simon says.
+// Create a variable to keep track of the current step in the sequence.
+// Implement a function to play a sound associated with a color.
+// Implement a function to add a new random color (or sound) to the sequence.
+// Implement a function to compare the player's input with the sequence.
+// Implement a function to start the game and play the Simon sequence.
+// Implement event listeners for user input (clicking the colored buttons).
+// Implement logic to check if the player's input matches the Simon sequence.
+// Display feedback to the player (e.g., success or failure).
