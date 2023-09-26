b86fcf8e69b6882b3b0ddd436e9a1705f7d57962
btlenny <btlenny@gmail.com>
2023-09-26 13:43:54 -0700

    tweaking working product

--- js/main.js
+++ js/main.js
@@ -1,133 +1,133 @@
 // Constants:
-const startButton = document.querySelector('#start-button')
-const levelDisplay = document.querySelector('h2');
-const topL = document.getElementById('tl');
-const topR = document.getElementById('tr');
-const bottomL= document.getElementById('bl');
-const bottomR = document.getElementById('br');
-const colors = ['tl', 'tr', 'bl', 'br'];
+const startButton = document.querySelector('#start-button'); // Get the start button element
+const levelDisplay = document.querySelector('h2'); // Get the level display element
+const topL = document.getElementById('tl'); // Get the top left tile element
+const topR = document.getElementById('tr'); // Get the top right tile element
+const bottomL= document.getElementById('bl'); // Get the bottom left tile element
+const bottomR = document.getElementById('br'); // Get the bottom right tile element
+
 
 // Variables:
-let computerSequence = [];
-let playerSequence = [];
-let level = 0;
+let computerSequence = []; // An array to store the computer's sequence
+let playerSequence = []; // An array to store the player's sequence
+let level = 0; // A variable to keep track of the game level
 
 // INTRO SONG
 function playIntroSong() {
+    // Create an audio element for the intro song and play it
     const introSong = new Audio('/audio/intro.wav'); 
     introSong.play();
-  }
-  
-//START BUTTON
-// 1a. Start button disapears after click and Level appears
+}
+
+// START BUTTON
+// Start button disappears after click and Level appears
 startButton.addEventListener('click', startGame);
 
 function startGame() {
-    startButton.classList.add('hidden'); 
-    const levelDisplay = document.querySelector('h2'); 
-    levelDisplay.style.visibility = 'visible'; 
+    startButton.classList.add('hidden'); // Hide the start button
+    const levelDisplay = document.querySelector('h2'); // Get the level display DOM
+    levelDisplay.style.visibility = 'visible'; // Make the level display visible
     playIntroSong();
     setTimeout(() => {
-    nextRound();
-    }, 5000);
+        nextRound();
+    }, 5000); // Delay before starting the next round (5 seconds)
 }
-    
-  
+
 // NEXT ROUND
 function nextRound(){
-  level++;
-  levelDisplay.textContent = `Level: ${level} of 20`;
-  generateComputerSequence();
-  setTimeout(() => {
-    playComputerSequence();
-  }, 1000)
-  
+    level++; //Increase leve by 1
+    levelDisplay.textContent = `Level: ${level} of 10`; // Update the level display
+    generateComputerSequence(); // Generate the computer's sequence
+    setTimeout(() => {
+        playComputerSequence();
+    }, 1000) // Delay before playing the computer's sequence (1 second)
 }
 
-//GENERATE RANDOM SEQUENCE
+// GENERATE RANDOM SEQUENCE
+const colors = ['tl', 'tr', 'bl', 'br']; // An array of color identifiers
 function generateComputerSequence() {
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  computerSequence.push(randomColor);
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]; // Randomly select a color
+    computerSequence.push(randomColor); // Add the color to the computer's sequence
 }
-
 
 // PLAY SEQUENCE
 const changeColorTone = {
-  tl: changeColorTopL,
-  tr: changeColorTopR,
-  bl: changeColorBottomL,
-  br: changeColorBottomR
+    tl: changeColorTopL,
+    tr: changeColorTopR,
+    bl: changeColorBottomL,
+    br: changeColorBottomR
 };
 function playComputerSequence() {
-  playerSequence = []; // Reset player sequence
-  let i = 0;
-  const interval = setInterval(() => {
-    const tileToHighlight = computerSequence[i];
-    changeColorTone[tileToHighlight](); // Call the corresponding function
-    i++;
-    if (i >= computerSequence.length) {
-      clearInterval(interval);
-      // Enable player input here
-    }
-  }, 1000); // Adjust the interval as needed
+    playerSequence = []; // Reset player sequence
+    let i = 0;
+    const interval = setInterval(() => {
+        const tileToHighlight = computerSequence[i];
+        changeColorTone[tileToHighlight](); // Call the corresponding function to highlight the tile
+        i++;
+        if (i >= computerSequence.length) {
+            clearInterval(interval);
+            // Enable player input here
+        }
+    }, 1000); // Adjust the interval as needed to control the pace of the sequence
 }
 
 // PLAYER INPUT
 topL.addEventListener('click', () => {
-  handleTileClick('tl');
+    handleTileClick('tl'); // Handle a click on the top left tile
 });
 
 topR.addEventListener('click', () => {
-  handleTileClick('tr');
+    handleTileClick('tr'); // Handle a click on the top right tile
 });
 
 bottomL.addEventListener('click', () => {
-  handleTileClick('bl');
+    handleTileClick('bl'); // Handle a click on the bottom left tile
 });
 
 bottomR.addEventListener('click', () => {
-  handleTileClick('br');
+    handleTileClick('br'); // Handle a click on the bottom right tile
 });
 
 function handleTileClick(tile) {
-  playerSequence.push(tile);
- 
-  if (!compareSequences()) {
-      // Player made a mistake, trigger game over logic
-      alert('Wrong color! Game over.');
-      gameOver();
-  } else if (playerSequence.length === computerSequence.length) {
-      // Player completed the round
-      setTimeout(nextRound, 1000); // Delay before starting the next round
-  }
+    playerSequence.push(tile); // Add the clicked tile to the player's sequence
+
+    if (!compareSequences()) {
+        // Player made a mistake, trigger game over logic
+        alert('Wrong color! Game over.');
+        gameOver();
+    } else if (playerSequence.length === computerSequence.length) {
+        // Player completed the round
+        setTimeout(nextRound, 1000); // Delay before starting the next round
+    }
 }
 
 function compareSequences() {
-  for (let i = 0; i < playerSequence.length; i++) {
-      if (playerSequence[i] !== computerSequence[i]) {
-          return false; // Player's input doesn't match computer's sequence
-      }
-  }
-  return true; // Player's input matches computer's sequence so far
+    for (let i = 0; i < playerSequence.length; i++) {
+        if (playerSequence[i] !== computerSequence[i]) {
+            return false; // Player's input doesn't match computer's sequence
+        }
+    }
+    return true; // Player's input matches computer's sequence so far
 }
 
 // END GAME
 function gameOver() {
-  // Implement game over logic (e.g., display a message, reset variables)
-  // You might want to show the start button again
-  startButton.classList.remove('hidden');
-  computerSequence = []; // Reset the computer's sequence
-  playerSequence = []; // Reset the player's sequence
-  level = 0; // Reset the level
-  levelDisplay.textContent = ''; // Clear the level display
-}
-function displayWinMessage() {
-  alert('Congratulations! You have won the game!');
+    // Implement game over logic (e.g., display a message, reset variables)
+    // You might want to show the start button again
+    startButton.classList.remove('hidden'); // Show the start button
+    computerSequence = []; // Reset the computer's sequence
+    playerSequence = []; // Reset the player's sequence
+    level = 0; // Reset the level
+    levelDisplay.textContent = ''; // Clear the level display
 }
 
-function nextRound() {
+function displayWinMessage() {
+    alert('Congratulations! You have won the game!'); // Display a win message
+}
+
+function handleWin() {
   level++;
-  if (level <= 2) {
+  if (level <= 10) {
       levelDisplay.textContent = `Level: ${level} of 20`;
       generateComputerSequence();
       playComputerSequence();
